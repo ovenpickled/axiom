200920ead6351c09f17ef8fc69ebfcbfb650dca0
ovenpickled <aryanghate29@gmail.com>
2026-05-12 11:43:57 +0530

    more navigations with keyboard keys

--- axiom.c
+++ axiom.c
@@ -325,7 +325,6 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
-
 void editorMoveCursor(int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
@@ -380,12 +379,19 @@ void editorProcessKeypress() {
       break;
 
     case END_KEY:
-      E.cx = E.screencols - 1;
+      if (E.cy < E.numrows)
+        E.cx = E.row[E.cy].size;
       break;
 
     case PAGE_UP:
     case PAGE_DOWN:
       {
+        if (c == PAGE_UP) {
+          E.cy = E.rowoff;
+        } else if (c == PAGE_DOWN) {
+          E.cy = E.rowoff + E.screenrows - 1;
+          if (E.cy > E.numrows) E.cy = E.numrows;
+        }
         int times = E.screenrows;
         while (times--)
           editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
